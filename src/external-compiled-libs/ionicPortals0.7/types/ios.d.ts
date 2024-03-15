6afc80d99fff2ff9deebdcad30e889ef55fda5ed
alexanderkolomiytsev <alexander.kolomiytsev@egym.com>
2024-03-15 17:33:18 +0200

    feat: init

--- src/external-compiled-libs/ionicPortals0.7/types/ios.d.ts
+++
@@ -1,6 +0,0 @@
-import { PortalMessage, PortalsPlugin, PortalSubscription, SubscribeOptions, SubscriptionCallback } from './definitions';
-export declare class PortalsIOS implements PortalsPlugin {
-    publish(message: PortalMessage): Promise<void>;
-    subscribe<T = unknown>(options: SubscribeOptions, callback: SubscriptionCallback<T>): Promise<PortalSubscription>;
-    unsubscribe(options: PortalSubscription): Promise<void>;
-}
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { getPortalsInitialContext } from './ionic-portals'
